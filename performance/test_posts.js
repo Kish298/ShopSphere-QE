11c749dd8c87feeb85c9c5e437d66f222c4f0fd2
Kish298 <kishanrsingh.2016@gmail.com>
2026-05-20 09:55:37 +0530

    Added performance testing

---
+++ performance/test_posts.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export default function () {
+
+    let res = http.get('https://jsonplaceholder.typicode.com/posts');
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
